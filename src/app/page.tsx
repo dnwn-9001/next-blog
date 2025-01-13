9705a6444afd02499e5b4104055655b39e84ba69
dnwn-9001 <j10511@hanmail.net>
2025-01-14 01:15:09 +0900

    star 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Star from "@/components/styles/star";
+
 export default function Page() {
-  return <div className="bg-black h-full"></div>;
+  return (
+    <div className="bg-black h-full">
+      <Star />
+    </div>
+  );
 }
---
+++ src/components/styles/star.tsx
@@ -0,0 +1,5 @@
+export default function Star() {
+  return (
+    <div className="w-1 h-1 rounded-full bg-white absolute top-24 left-24 overflow-hidden opacity-70  animate-twinkle"></div>
+  );
+}
