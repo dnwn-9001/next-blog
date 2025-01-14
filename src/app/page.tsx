3347d4ccb49a09d9c394d0e9ad727af9af4d1779
dnwn-9001 <j10511@hanmail.net>
2025-01-15 01:51:16 +0900

    star 컴포넌트 원하는만큼 보여주기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
 import Star from "@/components/styles/star";
 
 export default function Page() {
+  const starCount = 80; // 원하는 별의 개수
   return (
-    <div className="bg-black h-full">
-      <Star />
+    <div className="bg-black h-full relative">
+      {Array.from({ length: starCount }).map((_, index) => (
+        <Star key={index} />
+      ))}
     </div>
   );
 }
