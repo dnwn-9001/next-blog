d43a800d969f174258acfb6e76715d3466eff531
dnwn-9001 <j10511@hanmail.net>
2025-01-15 01:53:49 +0900

    relative 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Star from "@/components/styles/star";
 export default function Page() {
   const starCount = 80; // 원하는 별의 개수
   return (
-    <div className="bg-black h-full relative">
+    <div className="bg-black h-full">
       {Array.from({ length: starCount }).map((_, index) => (
         <Star key={index} />
       ))}
