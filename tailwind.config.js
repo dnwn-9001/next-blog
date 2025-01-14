9a93cc491d548ee9bc837f31010d8546cbcf9329
dnwn-9001 <j10511@hanmail.net>
2025-01-15 01:49:54 +0900

    별 스타일 및 애니메이션 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,38 @@ module.exports = {
           "0%, 100%": {
             // 애니메이션 시작과 끝에서 opacity를 0.7로 설정
             opacity: 0.7,
-            boxShadow: " 0 0 10px 3px rgb(255, 255, 255, 0.4)",
+            boxShadow: " 0 0 10px 3px rgb(255, 255, 255, 0.6)",
           },
-          "50%": { opacity: 1 }, // 애니메이션 중간에서 opacity를 1로 설정
+          "50%": {
+            opacity: 1,
+            // boxShadow: ' 0 0 10px 3px rgb(255, 255, 255, 0.7)',
+          }, // 애니메이션 중간에서 opacity를 1로 설정
         },
       },
       animation: {
         //정의된 키프레임 애니메이션을 사용할 수 있도록 설정
-        twinkle: "twinkle 1.5s infinite alternate",
-        // 'twinkle' 애니메이션을 1초 동안 무한 반복
+        twinkle1: "twinkle 1.5s infinite alternate",
+        // 'twinkle' 애니메이션을 1.5초 동안 무한 반복
         // ( alternate : 애니메이션이 끝에서 다시 시작할 때, 역방향으로 재생되도록 설정)
+        twinkle2: "twinkle 1s infinite alternate",
+        twinkle3: "twinkle 2s infinite alternate",
+        twinkle4: "twinkle 0.5s infinite alternate",
+      },
+      width: {
+        1: "1px",
+        2: "2px",
+        3: "3px",
+      },
+      height: {
+        1: "1px",
+        2: "2px",
+        3: "3px",
+      },
+      opacity: {
+        0.1: "0.1",
+        0.5: "0.5",
+        0.7: "0.7",
+        1: "1",
       },
     },
   },
