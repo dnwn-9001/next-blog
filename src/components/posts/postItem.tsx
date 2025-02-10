8457b9e7a9b22ac2ab03f23d853892084c8e5f05
dnwn-9001 <j10511@hanmail.net>
2025-02-11 00:42:02 +0900

    feat : 라디오 버튼 클릭 시 리스트 형태 다르게(임시)

--- src/components/posts/postItem.tsx
+++ src/components/posts/postItem.tsx
@@ -1,20 +1,41 @@
-// import React, { useState } from "react";
+"use client";
+import React, { useState } from "react";
+import RadioLabel from "../common/radioLabel";
 
 export default function PostItem() {
-  //const [itemType, setItemType] = useState("card");
+  const [itemType, setItemType] = useState("card");
 
   return (
-    <div className="w-10/12 h-full m-auto mt-32 flex flex-wrap">
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
-      <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+    <div className="w-10/12 m-auto">
+      <div className="m-auto mt-32 mb-6 flex justify-center">
+        <RadioLabel
+          label="카드형"
+          name="radio-1"
+          defaultChecked={true}
+          onClick={() => setItemType("card")}
+        />
+        <RadioLabel
+          label="리스트형"
+          name="radio-1"
+          onClick={() => setItemType("list")}
+        />
+      </div>
+      {itemType === "card" ? (
+        <div className="h-full flex flex-wrap justify-center">
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+          <div className="w-1/6 h-80 bg-slate-50 rounded-xl mx-3.5"></div>
+        </div>
+      ) : (
+        <h1>리스트형으로 들어와야함</h1>
+      )}
     </div>
   );
 }
