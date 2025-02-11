b36be3fdce834e27bf3739232848cb9bc601a71e
dnwn-9001 <j10511@hanmail.net>
2025-02-12 01:19:59 +0900

    refactor : 카드 아이템 컴포넌트로 분리리

---
+++ src/components/posts/cardTypeItem.tsx
@@ -0,0 +1,30 @@
+interface CardTypeItemProps {
+  title: string;
+  contents: string;
+  writer: string;
+  imgUrl?: string;
+}
+
+function CardTypeItem({
+  title,
+  contents,
+  writer,
+  imgUrl,
+}: CardTypeItemProps): React.ReactElement {
+  return (
+    <div className="w-[calc(25%-1rem)] card bg-base-100 shadow-xl">
+      <figure>
+        <img src={imgUrl} alt="게시글" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title.slice(0, 30)}</h2>
+        <p>{contents.slice(0, 60)}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">{writer}</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardTypeItem;
